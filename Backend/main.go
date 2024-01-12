ec1287fcf6cad90acaccb3866801412caf91b4fb
rakaaw <rakaaw3108@gmail.com>
2024-01-12 22:24:50 +0700

    add CORS policy

--- Backend/main.go
+++ Backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"donasila-repo/back-end/controllers"
 	"donasila-repo/back-end/initializers"
+	"github.com/gin-contrib/cors"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,6 +16,14 @@ func init() {
 func main() {
 
 	r := gin.Default()
+
+	// Apply CORS middleware
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowCredentials = true
+	config.AddAllowHeaders("Authorization")
+
+	r.Use(cors.New(config))
 
 	r.POST("/donasi/:donaturId", controllers.CreateDonasi)
 
